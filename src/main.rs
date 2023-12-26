bc0f4ff63331eaad165aa605a9e94cd5648502be
April <fynjyxbrjd@gmail.com>
2023-12-26 14:20:25 +0700

    refactor: make use of `use`, add `locale` wildcard

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use colored::Colorize;
-use std::collections::{HashSet, VecDeque};
-use std::io::{self, BufRead, Write};
-
 mod locale;
 mod macros;
+
+use colored::Colorize;
+use locale::*;
+use std::collections::{HashSet, VecDeque};
+use std::io::{self, BufRead, Write};
+use std::{process, fs, env};
 
 const WORD_HISTORY_BUFFER_LENGTH: usize = 10;
 
@@ -14,29 +16,29 @@ fn main() {
     let mut last_words_buffer: VecDeque<String> = VecDeque::new();
     let mut results_filname: Option<String> = None;
 
-    let mut args = std::env::args().skip(1);
+    let mut args = env::args().skip(1);
 
     while let Some(token) = args.next() {
         if token == "--save" {
             let filename = args.next().unwrap_or_else(|| {
-                err!(locale::ERR_MISSING_FILENAME);
-                std::process::exit(1);
+                err!(ERR_MISSING_FILENAME);
+                process::exit(1);
             });
             results_filname = Some(filename);
         } else if token == "--help" {
-            println!("{}", locale::HELP_STRING);
+            println!("{}", HELP_STRING);
             return;
         } else {
-            err!(locale::ERR_INVALID_CLI_ARGUMENT, token);
-            std::process::exit(1);
+            err!(ERR_INVALID_CLI_ARGUMENT, token);
+            process::exit(1);
         }
     }
 
-    info!(locale::INF_WELCOME);
+    info!(INF_WELCOME);
     let mut stdin = stdin.lock().lines();
 
     loop {
-        print!("{}", locale::PROMPT);
+        print!("{}", PROMPT);
         let _ = io::stdout().flush();
 
         let line = if let Some(res) = stdin.next() {
@@ -51,54 +53,54 @@ fn main() {
             continue;
         }
 
-        if query == locale::COMMAND_UNDO {
+        if query == COMMAND_UNDO {
             if let Some(last_word) = last_words_buffer.iter().last() {
                 words.remove(last_word);
-                info!(locale::INF_UNDO_WORD, &last_word);
+                info!(INF_UNDO_WORD, &last_word);
                 last_words_buffer.pop_back();
             } else {
-                err!(locale::ERR_END_OF_UNDO_BUFFER);
+                err!(ERR_END_OF_UNDO_BUFFER);
             }
             continue;
         }
 
-        if query == locale::COMMAND_EXIT {
-            info!(locale::INF_EXIT, words.len());
+        if query == COMMAND_EXIT {
+            info!(INF_EXIT, words.len());
             if let Some(filename) = results_filname {
                 let content = words
                     .iter()
                     .map(|w| &**w)
                     .collect::<Vec<&str>>()
                     .join("\n");
-                std::fs::write(filename, content).unwrap_or_else(|e| {
-                    err!(locale::ERR_FAILED_WRITE, e);
-                    std::process::exit(1);
+                fs::write(filename, content).unwrap_or_else(|e| {
+                    err!(ERR_FAILED_WRITE, e);
+                    process::exit(1);
                 });
             } else {
                 for item in words {
                     println!("{}", item);
                 }
             }
-            std::process::exit(0);
+            break;
         }
 
         if query.starts_with('!') {
-            err!(locale::ERR_INVALID_COMMAND);
+            err!(ERR_INVALID_COMMAND);
             continue;
         }
 
         if query.starts_with('?') {
-            println!("{}", locale::HELP_STRING);
+            println!("{}", HELP_STRING);
             continue;
         }
 
         if !query.chars().all(|c| c.is_alphabetic() || c == '-') {
-            err!(locale::ERR_INVALID_CHARACTER);
+            err!(ERR_INVALID_CHARACTER);
             continue;
         }
 
-        if !query.chars().any(|c| !locale::ILLEGAL_STARTING_CHARACTERS.contains(&c)) {
-            err!(locale::ERR_INVALID_WORD);
+        if !query.chars().any(|c| !ILLEGAL_STARTING_CHARACTERS.contains(&c)) {
+            err!(ERR_INVALID_WORD);
             continue;
         }
 
@@ -106,16 +108,16 @@ fn main() {
             let target_char = last_word
                 .chars()
                 .rev()
-                .find(|c| !locale::ILLEGAL_STARTING_CHARACTERS.contains(c))
+                .find(|c| !ILLEGAL_STARTING_CHARACTERS.contains(c))
                 .unwrap();
             if query.chars().next().unwrap() != target_char {
-                err!(locale::ERR_ILLEGAL_FIRST_CHARACTER, target_char);
+                err!(ERR_ILLEGAL_FIRST_CHARACTER, target_char);
                 continue;
             }
         }
 
         if !words.insert(query.clone()) {
-            err!(locale::ERR_WORD_REPEAT);
+            err!(ERR_WORD_REPEAT);
         } else {
             last_words_buffer.push_back(query);
             if last_words_buffer.len() > WORD_HISTORY_BUFFER_LENGTH {
