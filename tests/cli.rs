07f53aa273930716e71d546fa9f34d0a730d3add
April <fynjyxbrjd@gmail.com>
2024-01-04 15:28:10 +0700

    test: add some tests

---
+++ tests/cli.rs
@@ -0,0 +1,46 @@
+use assert_cmd::Command;
+use assert_fs::{NamedTempFile, assert::PathAssert};
+use predicates::prelude::*;
+use predicate::str::contains as str_contains;
+
+use word_chain_game::literals::*;
+
+#[test]
+fn invalid_argument() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("words")?;
+
+    cmd.arg("--foo").arg("bar");
+    cmd.assert()
+        .failure()
+        .stderr(str_contains(ERR_INVALID_CLI_ARGUMENT));
+
+    Ok(())
+}
+
+#[test]
+fn help_string() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("words")?;
+
+    cmd.arg("--help");
+    cmd.assert()
+        .success()
+        .stdout(str_contains(HELP_STRING));
+
+    Ok(())
+}
+
+#[test]
+fn save_to_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = NamedTempFile::new("result.txt")?;
+    let mut cmd = Command::cargo_bin("words")?;
+
+    cmd.arg("--save").arg(file.path()).write_stdin("fine\n!exit");
+    cmd.assert()
+        .success()
+        .stdout(str_contains(INF_WELCOME).and(str_contains(INF_EXIT)));
+
+    file.assert("fine");
+    file.close()?;
+
+    Ok(())
+}
